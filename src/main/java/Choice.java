d7d204f3c14fe6e9765fb1f3c5fef6b6877fdf47
Lucas <rochettelucas@gmail.com>
2021-03-07 13:52:57 +0100

    Choice class and json cleaning

--- src/main/java/Choice.java
+++ src/main/java/Choice.java
@@ -5,16 +5,8 @@ import java.util.Map;
 public class Choice {
 
     private String name;
-    private String actionOnFaction;
-    private String actionOnFactor;
-    private String effects;
     private Effect effectsObj;
 
-
-    public Choice(String name, String effects) {
-        this.name = name;
-        this.effects = effects;
-    }
 
     public Choice(String name, Effect actualEffect) {
         this.name = name;
@@ -30,35 +22,10 @@ public class Choice {
     }
 
 
-    public String getEffects() {
-        return effects;
-    }
-
-    protected void setEffects(String effects) {
-        this.effects = effects;
-    }
-
     public void setName(String name) {
         this.name = name;
     }
 
-
-
-    public String getActionOnFaction() {
-        return actionOnFaction;
-    }
-
-    protected void setActionOnFaction(String actionOnFaction) {
-        this.actionOnFaction = actionOnFaction;
-    }
-
-    public String getActionOnFactor() {
-        return actionOnFactor;
-    }
-
-    protected void setActionOnFactor(String actionOnFactor) {
-        this.actionOnFactor = actionOnFactor;
-    }
 
     protected void setEffectsObj(Effect effectsObj) {
         this.effectsObj = effectsObj;
